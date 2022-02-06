7ac917af6de63cbe91ade5658cfcbea0fbc85e87
Dengfeng Xia <dxia@twitter.com>
2022-02-05 20:23:17 -0800

    Change name

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer"
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>Bean Shop</h1>
       <nav
         style={{
           borderBottom: 'solid 1px',
